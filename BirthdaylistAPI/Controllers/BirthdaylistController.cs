91de1d469abb092869d9364c6cc73d14e271ef21
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-07-14 10:34:48 +0200

    added birthdaylist controller, tested in SSMS databse program if the connection worked, it worked

---
+++ BirthdaylistAPI/Controllers/BirthdaylistController.cs
@@ -0,0 +1,18 @@
+﻿using BirthdaylistAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdaylistAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthdaylistController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BirthdaylistController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
