42f3cfeb99567dab85288bc09ca5d62bac81ad31
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-07-14 09:13:13 +0200

    added all required fields/ variables, inluding each needed datatype

--- BirthdaylistAPI/Models/BirthdaylistAPI.cs
+++ BirthdaylistAPI/Models/BirthdaylistAPI.cs
@@ -2,4 +2,8 @@
 
 public class BirthdaylistAPI
 {
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required DateTime Birthday { get; set; }
+    public required bool ShouldCongratulate { get; set; }
 }
