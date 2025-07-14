e65fa2872becd7450d358bd243291daf5aed5b94
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-07-14 13:21:40 +0200

    inital commit

---
+++ BirthdaylistAPI/DTOs/BirthdaylistDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BirthdaylistAPI.DTOs
+{
+    public class BirthdaylistDTO
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required DateTime Birthday { get; set; }
+        public required bool ShouldCongratulate { get; set; }   
+    }
+}
